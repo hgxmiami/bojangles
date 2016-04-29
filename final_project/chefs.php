b62b9a9d7ad6da716b479a385687404154c6e2ac
hgxmiami <hgxmiami@aol.com>
2016-04-28 21:17:13 -0400

    final_project
    final

---
+++ final_project/chefs.php
@@ -0,0 +1,129 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Zentro</title>
+
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+<!--
+
+Template 2076 Zentro
+
+http://www.tooplate.com/view/2076-zentro
+
+-->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/nivo-lightbox.css">
+	<link rel="stylesheet" href="css/nivo_themes/default/default.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+
+
+<!-- navigation section -->
+<section class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+			</button>
+			<a href="#" class="navbar-brand">ZENTRO</a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav navbar-right">
+                <li><a href="index.php" class="smoothScroll">HOME</a></li>
+				<li><a href="foodgallery.php" class="smoothScroll">FOOD GALLERY</a></li>
+				<li><a href="specialmenu.php" class="smoothScroll">SPECIAL MENU</a></li>
+                <li><a href="chefs.php" class="smoothScroll">CHEFS</a></li>
+				
+			</ul>
+		</div>
+	</div>
+</section>
+
+
+<section id="team" class="parallax-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8 col-sm-12 text-center">
+				<h1 class="heading">Meet Zentro chefs</h1>
+				<hr>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.3s">
+				<img src="images/team1.jpg" class="img-responsive center-block" alt="team img">
+				<h4>Thanya</h4>
+				<h3>Main Chef</h3>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
+				<img src="images/team2.jpg" class="img-responsive center-block" alt="team img">
+				<h4>Lynda</h4>
+				<h3>Pizza Specialist</h3>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.9s">
+				<img src="images/team3.jpg" class="img-responsive center-block" alt="team img">
+				<h4>Jenny Ko</h4>
+				<h3>New Baker</h3>
+			</div>
+		</div>
+	</div>
+    
+    <div class="product-list">
+        <div class="row">
+        
+		<h2>Some Specials</h2>
+		
+		<ul class="specials">
+				<li>
+					<div class="image">
+						<a href="detail.php">
+						<img src="images/430_3150132.jpg" alt=" Chocolate Angelfood Cupcakes" width="190" height="130"/>
+						</a>
+					</div>
+					<div class="detail">
+						<p class="name"><a href="detail.html"> Chocolate Angelfood Cupcakes</a></p>
+						<p class="view"><a href="detail.html">purchase</a> | <a href="detail.html">view details >></a></p>
+					</div>
+				</li>
+				<li>
+					<div class="image">
+						<a href="detail.php">
+						<img src="images/430_3151480.scale_20.jpg" alt=" Fruit Salad" width="190" height="130"/>
+						</a>
+					</div>
+					<div class="detail">
+						<p class="name"><a href="detail.html"> Fruit Salad</a></p>
+						<p class="view"><a href="detail.html">purchase</a> | <a href="detail.html">view details >></a></p>
+					</div>
+				</li>
+				<li>
+					<div class="image">
+						<a href="detail.php">
+						<img src="images/700_3473780.jpg" alt=" Fudge" width="190" height="130"/>
+						</a>
+					</div>
+					<div class="detail">
+						<p class="name"><a href="detail.html"> Fudge</a></p>
+						<p class="view"><a href="detail.html">purchase</a> | <a href="detail.html">view details >></a></p>
+					</div>
+				</li>
+			</ul>
+	</div><!-- product-list -->
+    </div>
+    
+</section>
+
+    </body>
+</html>
